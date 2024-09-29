6eeab263c2dcf3d13a18fc312777fbdd84b44300
Daniel-gif-nub <danielferro01234@gmail.com>
2024-09-28 21:20:12 -0300

    Atualização 28/09

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -18,4 +18,32 @@ $(document).ready(function() {
         typeSpeed: 100,
         loop: true
     });
+
+    //Owl Carrossel;
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items:2,
+        margin: 30, 
+        autoplay: true,
+        autoplayTimeOut: 5000,
+        responsive: {
+            0: {
+                items:1
+            },
+            900: {
+                items:2
+            }
+        }
+    })
+
+    //One Page Scroll
+    $.scrollIt({
+        upKey: 38,             // key code to navigate to the next section
+        downKey: 40,           // key code to navigate to the previous section
+        easing: 'linear',      // the easing function for animation
+        scrollTime: 600,       // how long (in ms) the animation takes
+        activeClass: 'active', // class given to the active nav element
+        onPageChange: null,    // function(pageIndex) that is called when page is changed
+        topOffset: 0           // offste (in px) for fixed top navigation
+      });
 });
